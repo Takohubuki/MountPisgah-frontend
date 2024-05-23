fcc66f460cd94df0d17c444d0b7e52545710c870
zhaodh <zhaodh@hdu.edu.cn>
2024-05-23 21:18:32 +0800

    init home page

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,26 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <!-- <v-app>
-  <v-container fluid class="section" id="section1">
-    <div class="section-content">
-      <h1>第一屏</h1>
-      <p>这是一段示例文本</p>
-    </div>
-  </v-container>
-  </v-app>
+  <v-toolbar color="primary" dark fixed>
+    <v-toolbar-title>毗斯迦山</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn>首页</v-btn>
+    <v-btn>团契简介</v-btn>
+    <v-btn>团契信息</v-btn>
+    <v-btn>团契事工</v-btn>
+    <v-btn>团契动态</v-btn>
+    <v-spacer></v-spacer>
+  </v-toolbar>
 
-  <v-app>
-  <v-container fluid class="section" id="section2">
-    <div class="section-content">
-      <h1>第二屏</h1>
-      <p>这是二段示例文本</p>
-    </div>
-  </v-container>
-  </v-app> -->
-  <!-- <HelloWorld msg="Vite + Vue" /> -->
   <div class="section" id="section1">
     <div class="container section-content">
-      <h1>第一屏</h1>
-      <p>这是一段示例文本</p>
+      <v-carousel hide-delimiters>
+        <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/docks.jpg" cover></v-carousel-item>
+
+        <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg" cover></v-carousel-item>
+
+        <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" cover></v-carousel-item>
+      </v-carousel>
     </div>
   </div>
 

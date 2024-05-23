83f440730d64443752724d2bef7431c423038ff4
zhaodh <zhaodh@hdu.edu.cn>
2024-05-23 19:56:51 +0800

    init home page

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,38 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <!-- <v-app>
+  <v-container fluid class="section" id="section1">
+    <div class="section-content">
+      <h1>第一屏</h1>
+      <p>这是一段示例文本</p>
+    </div>
+  </v-container>
+  </v-app>
+
+  <v-app>
+  <v-container fluid class="section" id="section2">
+    <div class="section-content">
+      <h1>第二屏</h1>
+      <p>这是二段示例文本</p>
+    </div>
+  </v-container>
+  </v-app> -->
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+  <div class="section" id="section1">
+    <div class="container section-content">
+      <h1>第一屏</h1>
+      <p>这是一段示例文本</p>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
+  <div class="section" id="section2">
+    <div class="container section-content">
+      <h1>第二屏</h1>
+      <p>这是二段示例文本</p>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
